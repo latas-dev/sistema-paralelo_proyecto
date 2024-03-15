ea59bfd8b98a927475eb7106deda0164459b1726
Victor Treviño <victortrevinovilla@gmail.com>
2024-03-15 11:01:17 -0600

    Modificado por victor

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,6 +1,10 @@
 
+
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("¡Hola, mundo!");
+		
+		System.out.println("ï¿½Hola, mundo!");
+		
+		System.out.println("HOLA MI NOMBRE ES VICTOR ");
 	}
 }
